a567a982eaf5bf52e56daa3779df61c62788da8b
Jacek Gębal <jgebal@gmail.com>
2017-04-18 00:56:51 +0100

    POC - sonar test reporter

--- test/install.sh
+++ test/install.sh
@@ -2,6 +2,18 @@
 
 set -ev
 
+#adding sonar test reporter
+sqlplus -L -S / <<SQL
+alter session set current_schema=ut3;
+
+@@test/ut_sonar_test_reporter.tps
+@@test/ut_sonar_test_reporter.tpb
+create public synonym ut_sonar_test_reporter for ut_sonar_test_reporter;
+grant execute on ut_sonar_test_reporter to public;
+
+exit
+SQL
+
 sqlplus -L -S ${DB_USER}/${DB_PASS} <<SQL
 whenever sqlerror exit failure rollback
 whenever oserror  exit failure rollback
--- test/run.sql
+++ test/run.sql
@@ -8,20 +8,47 @@ set long 200000000
 set longchunksize 1000000
 set serveroutput on size unlimited format truncated
 set arraysize 50
-set termout off
-spool sonar-coverage.xml
 
-select *
-  from table(ut.run(
-    ut_coverage_sonar_reporter(
-      a_file_paths => ut_varchar2_list(
+
+var sonar_test_reporter_id  varchar2(32);
+var sonar_coverage_reporter_id varchar2(32);
+declare
+  l_reporter  ut_reporter_base;
+  l_reporters ut_reporters := ut_reporters();
+begin
+
+  l_reporter := ut_coverage_sonar_reporter(
+    a_file_paths => ut_varchar2_list(
         'source/award_bonus/award_bonus.prc',
         'source/remove_rooms_by_name/remove_rooms_by_name.prc',
         'source/between_string/betwnstr.fnc'
-      )
-    )
-  ));
+      ));
+  l_reporters.extend;
+  l_reporters(l_reporters.last) := l_reporter;
+  :sonar_coverage_reporter_id := l_reporter.reporter_id;
 
+  l_reporter := ut_sonar_test_reporter(
+    a_file_paths => ut_varchar2_list(
+        'test/award_bonus/test_award_bonus.pkb',
+        'test/remove_rooms_by_name/test_remove_rooms_by_name.pkb',
+        'test/between_string/test_betwnstr.pkb'
+      ) );
+  l_reporters.extend;
+  l_reporters(l_reporters.last) := l_reporter;
+  :sonar_test_reporter_id := l_reporter.reporter_id;
+
+  ut_runner.run(user,l_reporters);
+end;
+/
+
+set termout off
+
+spool sonar-coverage.xml
+select * from table( ut_output_buffer.get_lines(:sonar_coverage_reporter_id));
+spool off
+
+spool sonar-test-results.xml
+select * from table( ut_output_buffer.get_lines(:sonar_test_reporter_id));
 spool off
 
 exit
