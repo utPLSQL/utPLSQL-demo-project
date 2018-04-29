2e8e7e8ad00986087025cdb586217704298d3191
Jacek Gebal <jgebal@users.noreply.github.com>
2018-04-30 00:37:07 +0100

    Fixed connect string for XE

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-utPLSQL-cli/bin/utplsql run ${DB_USER}/${DB_PASS}@${ORACLE_SID} \
+utPLSQL-cli/bin/utplsql run ${DB_USER}/${DB_PASS}@127.0.0.1:1521:${ORACLE_SID} \
 -source_path=source -test_path=test \
 -f=ut_documentation_reporter  -c \
 -f=ut_coverage_sonar_reporter -o=coverage.xml \
