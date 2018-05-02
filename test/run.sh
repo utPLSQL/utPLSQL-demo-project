e45bd261c79e3c15f7c533e726095e6517c0cbe5
Jacek Gebal <jgebal@users.noreply.github.com>
2018-05-02 01:39:10 +0100

    Fixing builds for versions of utPLSQL that didn't keep version no in git repo.

--- test/run.sh
+++ test/run.sh
@@ -7,4 +7,4 @@ utPLSQL-cli/bin/utplsql run ${DB_USER}/${DB_PASS}@127.0.0.1:1521:${ORACLE_SID} \
 -f=ut_documentation_reporter  -c \
 -f=ut_coverage_sonar_reporter -o=coverage.xml \
 -f=ut_sonar_test_reporter     -o=test_results.xml \
---failure-exit-code=0 -scc
+--failure-exit-code=0
