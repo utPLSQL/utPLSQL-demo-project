7c86299804f7d6b373e16c9b0ab91d86be5e16a0
Jacek Gebal <jgebal@users.noreply.github.com>
2019-05-16 22:45:53 +0100

    Fixing sqlcl connect string

--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -4,9 +4,9 @@ set -ev
 
 cd $UTPLSQL_DIR/source
 
-${SQLCLI} -S -L oracle/${ORACLE_PWD} AS SYSDBA @install_headless.sql
+${SQLCLI} -S -L sys/${ORACLE_PWD}@//${CONNECTION_STR} AS SYSDBA @install_headless.sql
 
-${SQLCLI} -L -S oracle/${ORACLE_PWD} AS SYSDBA <<SQL
-grant select any dictionary to ut3;
-exit
-SQL
+#${SQLCLI} -L -S sys/${ORACLE_PWD}@//${CONNECTION_STR} AS SYSDBA <<SQL
+#grant select any dictionary to ut3;
+#exit
+#SQL
--- .travis/setup_account.sh
+++ .travis/setup_account.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-${SQLCLI} -L -S oracle/${ORACLE_PWD} AS SYSDBA <<SQL
+${SQLCLI} -L -S sys/${ORACLE_PWD}@//${CONNECTION_STR} AS SYSDBA <<SQL
 create user ${DB_USER} identified by ${DB_PASS} quota unlimited on USERS default tablespace USERS;
 
 grant create session, create procedure, create type, create table, create sequence, create view to ${DB_USER};
