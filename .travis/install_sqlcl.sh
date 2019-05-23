4996fdde1237eee2fd3f8e17ce9928269b482b7a
Jacek Gebal <jgebal@users.noreply.github.com>
2019-05-23 01:00:22 +0100

    Cleanup

--- .travis/install_sqlcl.sh
+++ .travis/install_sqlcl.sh
@@ -1,20 +1,66 @@
 #!/bin/bash
 set -e
 
+###############################3
+# Declarations
+###############################3
+SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 SQLCL_FILE=sqlcl-18.3.0.259.2029.zip
-cd .travis
+AGREEMENT_URL="http://www.oracle.com/technetwork/developer-tools/sqlcl/downloads/index.html"
+DOWNLOAD_URL="https://download.oracle.com/otn/java/sqldeveloper/${SQLCL_FILE}"
+ZIP_TARGET_DIR=""
+OTN_USER=""
+OTN_PASS=""
+OUTPUT_DIR=""
 
-# Download if not present on cache dir.
-if [ ! -f $CACHE_DIR/$SQLCL_FILE ]; then
+# Call the casperjs script to return the download url. Then download the file using curl.
+downloadFile() {
+    user=$1
+    pass=$2
+    agreementUrl=$3
+    downloadUrl=$4
+    outputFile=$5
+    echo "Signing-in"
+    downloadUrl=$(exec casperjs ${SCRIPT_DIR}/download.js ${user} ${pass} ${agreementUrl} ${downloadUrl})
+    downloadUrl=${downloadUrl%$'\r'}
+    echo "DownloadURL: $downloadUrl"
+    curl -o ${outputFile} -L "$downloadUrl"
+}
+
+###############################3
+# Main script
+###############################3
+
+#Evaluate script input parameters
+while getopts "p:u:d:o:" OPTNAME; do
+    case "${OPTNAME}" in
+        "d") ZIP_TARGET_DIR="${OPTARG}" ;;
+        "u") OTN_USER="${OPTARG}" ;;
+        "p") OTN_PASS="${OPTARG}" ;;
+        "o") OUTPUT_DIR="${OPTARG}" ;;
+    esac
+done
+
+#Check parameters
+if [[ "${ZIP_TARGET_DIR}" == "" ]]; then
+    ZIP_TARGET_DIR="."
+fi
+if [[ "$OTN_USER" == "" ]] || [[ "$OTN_PASS" == "" ]]; then
+    echo "Error: Oracle OTN username/password not specified."
+    exit 1
+fi
+if [[ "$OUTPUT_DIR" == "" ]]; then
+    echo "Error: Output directory not specified"
+    exit 1
+fi
+
+if [[ ! -f "${ZIP_TARGET_DIR}/${SQLCL_FILE}" ]]; then
     npm install -g phantomjs-prebuilt casperjs
-    bash download.sh -p sqlcl
-    mv $SQLCL_FILE $CACHE_DIR
+    echo "Downloading sqlcl from Oracle"
+    downloadFile ${OTN_USER} ${OTN_PASS} ${AGREEMENT_URL} ${DOWNLOAD_URL} "${ZIP_TARGET_DIR}/${SQLCL_FILE}"
 else
     echo "Installing sqlcl from cache..."
 fi;
 
-# Install sqlcl.
-unzip -q $CACHE_DIR/$SQLCL_FILE -d $HOME
-
-# Check if it is installed correctly.
-$SQLCLI -v
+echo "Unzipping sqlcl into: ${ZIP_TARGET_DIR}"
+unzip -q "${ZIP_TARGET_DIR}/${SQLCL_FILE}" -d ${OUTPUT_DIR}
--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -2,11 +2,6 @@
 
 set -ev
 
-cd $UTPLSQL_DIR/source
+cd ${UTPLSQL_DIR}/source
 
-${SQLCLI} -S -L sys/${ORACLE_PWD}@//${CONNECTION_STR} AS SYSDBA @install_headless.sql
-
-#${SQLCLI} -L -S sys/${ORACLE_PWD}@//${CONNECTION_STR} AS SYSDBA <<SQL
-#grant select any dictionary to ut3;
-#exit
-#SQL
+sql -S -L sys/${ORACLE_SYS_PASS}@//${CONNECTION_STR} AS SYSDBA @install_headless.sql
