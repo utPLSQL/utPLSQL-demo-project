b11066b48bc163c0897d99c8e287dfde79e9d20e
Jacek Gebal <jgebal@users.noreply.github.com>
2019-07-14 16:04:41 +0100

    Added timezone env variable. Removed quotes from user/pass variables.

--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -3,6 +3,5 @@
 set -ev
 
 cd ${UTPLSQL_DIR}/source
-echo "sys password is: ${DB_SYS_PASSWORD}"
-echo "sys password is: $DB_SYS_PASSWORD"
+
 sql -S -L sys/${DB_SYS_PASSWORD}@//${CONNECTION_STR} AS SYSDBA @install_headless.sql
