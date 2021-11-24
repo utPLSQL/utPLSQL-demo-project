bc84f112aee1ad6ecafed7bf6e520359acf4f986
Jacek Gebal <jgebal@users.noreply.github.com>
2021-11-24 21:33:59 +0200

    Fixing build

--- scripts/2_install_demo_project.sh
+++ scripts/2_install_demo_project.sh
@@ -1,3 +1,7 @@
 docker run --rm -v $(pwd):/work -w /work/  --network host \
     --entrypoint sqlplus gvenzl/oracle-xe:18-slim \
-    sys/oracle@//127.0.0.1:1521/XE as sysdba @source/install.sql
+    sys/oracle@//127.0.0.1:1521/XE as sysdba @source/create_user.sql
+
+docker run --rm -v $(pwd):/work -w /work/  --network host \
+    --entrypoint sqlplus gvenzl/oracle-xe:18-slim \
+    ut3_demo/ut3_demo@//127.0.0.1:1521/XE as sysdba @source/install.sql
