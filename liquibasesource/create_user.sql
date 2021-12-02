a4b8b2d15be7a1333c426d9b44f247e6d87ab6ff
Lukasz Wasylow <lwasylow@gmail.com>
2021-12-02 11:59:45 +0000

    Adding pipeline

--- liquibasesource/create_user.sql
+++
@@ -1,6 +0,0 @@
-create user ut3_demo identified by ut3_demo quota unlimited on USERS default tablespace USERS;
-
-grant create session, create procedure, create type, create table, create sequence, create view to ut3_demo;
-grant select any dictionary to ut3_demo;
-
-exit
