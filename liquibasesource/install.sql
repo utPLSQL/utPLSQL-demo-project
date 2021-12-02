f98cc5a20084e362fe5e9f8e7b3d904db8642c46
Lukasz Wasylow <lwasylow@gmail.com>
2021-12-02 11:35:20 +0000

    Adding pipeline

---
+++ liquibasesource/install.sql
@@ -0,0 +1,6 @@
+@source/award_bonus/employees_test.sql
+@source/award_bonus/award_bonus.prc
+@source/between_string/betwnstr.fnc
+@source/remove_rooms_by_name/rooms.sql
+@source/remove_rooms_by_name/remove_rooms_by_name.prc
+exit
