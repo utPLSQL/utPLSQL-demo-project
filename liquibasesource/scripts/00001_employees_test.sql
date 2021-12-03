590c77698fd788d32724806d98c5f66c27f5e77a
Lukasz Wasylow <lwasylow@gmail.com>
2021-12-03 09:52:10 +0000

    Adding pipeline

---
+++ liquibasesource/scripts/00001_employees_test.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset utplsql:employees_test
+create table employees_test (employee_id number primary key, commission_pct number, salary number);
+insert into employees_test values (1001, 0.2, 8400);
+insert into employees_test values (1002, 0.25, 6000);
+insert into employees_test values (1003, 0.3, 5000);
+-- next employee is not in the sales department, thus is not on commission.
+insert into employees_test values (1004, null, 10000);
+commit;
+
+--rollback drop table employees_test;
