952d354345b86f12aef94525271e87246b1b4906
Lukasz Wasylow <lwasylow@gmail.com>
2021-12-08 23:34:30 +0000

    Update code to work for travis

--- source/install.sql
+++ source/install.sql
@@ -1,6 +1,6 @@
-@source/award_bonus/employees_test.sql
-@source/award_bonus/award_bonus.prc
-@source/between_string/betwnstr.fnc
-@source/remove_rooms_by_name/rooms.sql
-@source/remove_rooms_by_name/remove_rooms_by_name.prc
+@source/award_bonus/V1.01__employees_test.sql
+@source/award_bonus/R__award_bonus.prc
+@source/between_string/R__betwnstr.fnc
+@source/remove_rooms_by_name/V1.02__rooms.sql
+@source/remove_rooms_by_name/R__remove_rooms_by_name.prc
 exit
