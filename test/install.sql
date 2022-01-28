02ea063b478ef5afb48873aa14bb28c3ea21fd2b
Jacek Gebal <jgebal@users.noreply.github.com>
2022-01-28 23:58:46 +0200

    Checking test errors

--- test/install.sql
+++ test/install.sql
@@ -1,7 +1,10 @@
 @test/award_bonus/test_award_bonus.pks
 @test/award_bonus/test_award_bonus.pkb
+show errors
 @test/between_string/test_betwnstr.pks
 @test/between_string/test_betwnstr.pkb
+show errors
 @test/remove_rooms_by_name/test_remove_rooms_by_name.pks
 @test/remove_rooms_by_name/test_remove_rooms_by_name.pkb
+show errors
 exit
