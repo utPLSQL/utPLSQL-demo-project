21d18321869203e5beb24d0abbf359a00cb81771
Jacek Gebal <jgebal@users.noreply.github.com>
2022-01-29 00:04:21 +0200

    Fixing the demo install

--- scripts/2_install_demo_project.sh
+++ scripts/2_install_demo_project.sh
@@ -4,4 +4,4 @@ docker run --rm -v $(pwd):/work -w /work/  --network host \
 
 docker run --rm -v $(pwd):/work -w /work/  --network host \
     --entrypoint sqlplus gvenzl/oracle-xe:18-slim \
-    ut3_demo/ut3_demo@//127.0.0.1:1521/XE as sysdba @source/install.sql
+    ut3_demo/ut3_demo@//127.0.0.1:1521/XE @source/install.sql
