1b16d5c8e47270cd313028a0578784e095d60bea
Lukasz Wasylow <lwasylow@gmail.com>
2023-09-16 00:29:31 +0100

    Fixing regex for source

--- scripts/4_run_tests.sh
+++ scripts/4_run_tests.sh
@@ -3,7 +3,14 @@
 set -ev
 
 utPLSQL-cli/bin/utplsql run ut3_demo/ut3_demo@//127.0.0.1:1521/XE \
-  -source_path=source -test_path=test \
+  -source_path=source -regex_expression="/(\w+)/(\w+)/(R__)(\w+)\.(\w+)\.(\w+)$" \
+  -owner_subexpression=4 \
+  -name_subexpression=5 \
+  -type_subexpression=6 \
+  -test_path=test -regex_expression="/(\w+)/(\w+)/(\w+)\.(\w+)\.(\w+)$" \
+  -owner_subexpression=3 \
+  -name_subexpression=4 \
+  -type_subexpression=5 \
   -f=ut_documentation_reporter  -c \
   -f=ut_coverage_sonar_reporter -o=coverage.xml \
   -f=ut_sonar_test_reporter     -o=test_results.xml \
--- test/install.sql
+++ test/install.sql
@@ -1,10 +1,10 @@
-@test/award_bonus/test_award_bonus.pks
-@test/award_bonus/test_award_bonus.pkb
+@test/award_bonus/ut3_demo.test_award_bonus.pks
+@test/award_bonus/ut3_demo.test_award_bonus.pkb
 show errors
-@test/between_string/test_betwnstr.pks
-@test/between_string/test_betwnstr.pkb
+@test/between_string/ut3_demo.test_betwnstr.pks
+@test/between_string/ut3_demo.test_betwnstr.pkb
 show errors
-@test/remove_rooms_by_name/test_remove_rooms_by_name.pks
-@test/remove_rooms_by_name/test_remove_rooms_by_name.pkb
+@test/remove_rooms_by_name/ut3_demo.test_remove_rooms_by_name.pks
+@test/remove_rooms_by_name/ut3_demo.test_remove_rooms_by_name.pkb
 show errors
 exit
