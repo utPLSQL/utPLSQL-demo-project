eaf47d264ca174f5d8992b0d88c117fe3cde7457
Lukasz Wasylow <lwasylow@gmail.com>
2023-09-21 18:13:30 +0100

    test path

--- scripts/4_run_tests.sh
+++ scripts/4_run_tests.sh
@@ -3,11 +3,11 @@
 set -ev
 
 utPLSQL-cli/bin/utplsql run ut3_demo/ut3_demo@//127.0.0.1:1521/XE \
-  -source_path=source -regex_expression="/(\w+)/(\w+)/(R__)(\w+)\.(\w+)\.(\w+)$" \
+  -source_path=source -regex_expression="(\w+)/(\w+)/(R__)(\w+)\.(\w+)\.(\w+)$" \
   -owner_subexpression=4 \
   -name_subexpression=5 \
   -type_subexpression=6 \
-  -test_path=test -regex_expression="/(\w+)/(\w+)/(\w+)\.(\w+)\.(\w+)$" \
+  -test_path=test -regex_expression="(\w+)/(\w+)/(\w+)\.(\w+)\.(\w+)$" \
   -owner_subexpression=3 \
   -name_subexpression=4 \
   -type_subexpression=5 \
